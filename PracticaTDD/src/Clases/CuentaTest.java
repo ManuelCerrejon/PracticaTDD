860ebe5694875c61ebc7d5fca5db6070885a7c11
manuc <manuc@LAPTOP-1MM8CT3Q>
2022-10-25 10:41:01 +0200

    Version 1

---
+++ PracticaTDD/src/Clases/CuentaTest.java
@@ -0,0 +1,44 @@
+package Clases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CuentaTest {
+	
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		Cuenta ct12345 = Cuenta("12345",50);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngreso() {
+		
+	}
+
+	@Test
+	void testRetirar() {
+		ct12345.ingreso(50);
+		AssertEquals(100, ct12345.getSaldo(50));
+	}
+
+}
